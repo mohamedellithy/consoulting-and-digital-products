a48619b9c33b38bf3266e0fa67f34fabf3981dc4
mohamedellithy <develomohamed22@gmail.com>
2023-08-06 17:43:10 +0300

    djdjdsaewds

--- resources/views/partials/media_list_1.blade.php
+++ resources/views/partials/media_list_1.blade.php
@@ -4,12 +4,14 @@
             <video style="width:100%;height:100%" controls>
                 <source src="{{ upload_assets($media) }}" type="{{ $media->type }}"></source>
             </video>
-        @elseif('application' == formateMediaType($media->type)[0])
+        @elseif('pdf' == formateMediaType($media->type)[1])
             <i style="font-size: 120px;" class='bx bxs-file-pdf'></i>
         @elseif('text' == formateMediaType($media->type)[0])
             <i style="font-size: 120px;" class='bx bxs-file-txt'></i>
         @elseif('image' == formateMediaType($media->type)[0])
             <img src="{{ upload_assets($media) }}" class="list-item-image"/>
+        @elseif('vnd.openxmlformats-officedocument.spreadsheetml.sheet' == formateMediaType($media->type)[1])
+            <img src="{{ asset('assets/img/icons/xlsx.png') }}" class="list-item-image"/>
         @else
             <i style="font-size: 120px;" class='bx bxs-file-blank'></i>
         @endif
