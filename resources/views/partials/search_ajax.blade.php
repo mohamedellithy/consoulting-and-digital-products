730b5dd6cfb2782cf9ca6834d47c70e30424a318
mohamedellithy <develomohamed22@gmail.com>
2023-12-09 23:46:08 +0200

    djwwsdasssdssssssssddسdsssdddيs

--- resources/views/partials/search_ajax.blade.php
+++ resources/views/partials/search_ajax.blade.php
@@ -1,4 +1,4 @@
-<ul class="search-result" style="padding:20px;">
+<ul class="search-result">
     @foreach($results as $result)
         <li style="text-align: right;">
             <h4>{{ $result->name }}</h4>
@@ -50,3 +50,11 @@
         </li>
     @endforeach
 </ul>
+
+<style>
+    .search-result{
+        padding: 20px;
+        height: 300px;
+        overflow-y: scroll;
+    }
+</style>
