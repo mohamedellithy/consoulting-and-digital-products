62ce01a245fed570672194d1a9d7459a051a17fa
mohamedellithy <develomohamed22@gmail.com>
2023-12-09 23:13:06 +0200

    djwwsdasssdssssss

--- resources/views/layouts/master_front.blade.php
+++ resources/views/layouts/master_front.blade.php
@@ -154,8 +154,7 @@ $logo_url = upload_assets(get_settings('website_logo'),true);
         //window.frames["myiframe"].document.oncontextmenu = function(){ return false; };
     </script>
     <script>
-        alert('hi');
-        jQuery('#search').keyup(function(e){
+        jQuery('body').on('keyup','#search',function(e){
             alert('hi');
         });
     </script>
