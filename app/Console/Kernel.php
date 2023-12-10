b6b3130b11562086977a2dec89c95bb47d61c7ae
mohamedellithy <develomohamed22@gmail.com>
2023-12-10 16:47:30 +0200

    djddssd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('sitemap:generate-urls')->daily();
-        $schedule->command('currency:convertor'); //->hourly();
+        $schedule->command('currency:convertor')->everyTenHours();
         // $schedule->command('inspire')->hourly();
     }
 
